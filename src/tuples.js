a4d5b6e0ce91bc30fa29980545e5219bbd515800
swiftaff <swiftaff@gmail.com>
2020-02-20 22:05:29 +0800

    getBool_tuplesAreEqual, tuple_add

--- src/tuples.js
+++ src/tuples.js
@@ -1,9 +1,27 @@
-function getBool_tuple4ObjIsPoint(tuple4Obj) {
-    return tuple4Obj.w === 1;
+const EPSILON = 0.00001;
+
+function getBool_tupleIsPoint(tuple) {
+    return tuple.w === 1;
 }
 
-function getBool_tuple4ObjIsVector(tuple4Obj) {
-    return tuple4Obj.w === 0;
+function getBool_tupleIsVector(tuple) {
+    return tuple.w === 0;
+}
+
+function getBool_tuplesAreEqual(a, b) {
+    return (
+        Math.abs(a.x - b.x) < EPSILON && Math.abs(a.y - b.y) < EPSILON && Math.abs(a.z - b.z) < EPSILON && a.w === b.w
+    );
+}
+
+function tuple_add(a, b) {
+    let tuple = { x: a.x + b.x, y: a.y + b.y, z: a.z + b.z, w: a.w + b.w };
+    if (tuple.w === 2) {
+        console.error("tuple_add: can't add two points");
+        return false;
+    } else {
+        return tuple;
+    }
 }
 
 function tuple(x, y, z, w) {
@@ -18,4 +36,12 @@ function vector(x, y, z) {
     return { x, y, z, w: 0 };
 }
 
-module.exports = { tuple, point, vector, getBool_tuple4ObjIsPoint, getBool_tuple4ObjIsVector };
+module.exports = {
+    tuple,
+    point,
+    vector,
+    getBool_tupleIsPoint,
+    getBool_tupleIsVector,
+    getBool_tuplesAreEqual,
+    tuple_add
+};
--- test/tuples.test.js
+++ test/tuples.test.js
@@ -1,4 +1,12 @@
-const { tuple, point, vector, getBool_tuple4ObjIsPoint, getBool_tuple4ObjIsVector } = require("../src/tuples.js");
+const {
+    tuple,
+    point,
+    vector,
+    getBool_tupleIsPoint,
+    getBool_tupleIsVector,
+    getBool_tuplesAreEqual,
+    tuple_add
+} = require("../src/tuples.js");
 
 test("A tuple with w=1.0 is a point", function() {
     let a = tuple(4.3, -4.2, 3.1, 1.0);
@@ -6,8 +14,8 @@ test("A tuple with w=1.0 is a point", function() {
     expect(a.y).toBe(-4.2);
     expect(a.z).toBe(3.1);
     expect(a.w).toBe(1.0);
-    expect(getBool_tuple4ObjIsPoint(a)).toBe(true);
-    expect(getBool_tuple4ObjIsVector(a)).toBe(false);
+    expect(getBool_tupleIsPoint(a)).toBe(true);
+    expect(getBool_tupleIsVector(a)).toBe(false);
 });
 
 test("A tuple with w=0 is a point", function() {
@@ -16,16 +24,52 @@ test("A tuple with w=0 is a point", function() {
     expect(a.y).toBe(-4.2);
     expect(a.z).toBe(3.1);
     expect(a.w).toBe(0.0);
-    expect(getBool_tuple4ObjIsPoint(a)).toBe(false);
-    expect(getBool_tuple4ObjIsVector(a)).toBe(true);
+    expect(getBool_tupleIsPoint(a)).toBe(false);
+    expect(getBool_tupleIsVector(a)).toBe(true);
 });
 
 test("point() creates tuples with w=1", function() {
-    expect(getBool_tuple4ObjIsPoint(point(4, -4, 3))).toBe(true);
-    expect(getBool_tuple4ObjIsVector(point(4, -4, 3))).toBe(false);
+    expect(getBool_tupleIsPoint(point(4, -4, 3))).toBe(true);
+    expect(getBool_tupleIsVector(point(4, -4, 3))).toBe(false);
 });
 
 test("vector() creates tuples with w=0", function() {
-    expect(getBool_tuple4ObjIsPoint(vector(4, -4, 3))).toBe(false);
-    expect(getBool_tuple4ObjIsVector(vector(4, -4, 3))).toBe(true);
+    expect(getBool_tupleIsPoint(vector(4, -4, 3))).toBe(false);
+    expect(getBool_tupleIsVector(vector(4, -4, 3))).toBe(true);
+});
+
+test("tuples are equal: if exactly the same", function() {
+    expect(getBool_tuplesAreEqual(point(4, -4, 3), point(4, -4, 3))).toBe(true);
+    expect(getBool_tuplesAreEqual(vector(4, -4, 3), vector(4, -4, 3))).toBe(true);
+});
+
+test("tuples are NOT equal: if different", function() {
+    expect(getBool_tuplesAreEqual(point(4, -4, 3), point(3, -2, 1))).toBe(false);
+    expect(getBool_tuplesAreEqual(vector(4, -4, 3), vector(3, -2, 1))).toBe(false);
+});
+
+test("tuples are equal: if difference is less than EPSILON", function() {
+    expect(getBool_tuplesAreEqual(point(4.000001, -4.000001, 3.000001), point(4, -4, 3))).toBe(true);
+    expect(getBool_tuplesAreEqual(vector(4.000001, -4.000001, 3.000001), vector(4, -4, 3))).toBe(true);
+});
+
+test("tuples are NOT equal: if difference is more than EPSILON", function() {
+    expect(getBool_tuplesAreEqual(point(4.0001, -4.0001, 3.0001), point(4, -4, 3))).toBe(false);
+    expect(getBool_tuplesAreEqual(vector(4.0001, -4.0001, 3.0001), vector(4, -4, 3))).toBe(false);
+});
+
+test("adding two tuples: point + vector = point", function() {
+    expect(getBool_tuplesAreEqual(tuple_add(point(3, -2, 5), vector(-2, 3, 1)), point(1, 1, 6))).toBe(true);
+});
+
+test("adding two tuples: vector + vector = vector", function() {
+    expect(getBool_tuplesAreEqual(tuple_add(vector(3, -2, 5), vector(-2, 3, 1)), vector(1, 1, 6))).toBe(true);
+});
+
+test("adding two tuples: vector + point = vector", function() {
+    expect(getBool_tuplesAreEqual(tuple_add(vector(3, -2, 5), point(-2, 3, 1)), point(1, 1, 6))).toBe(true);
+});
+
+test("adding two tuples: point + point = false (and console error)", function() {
+    expect(tuple_add(point(3, -2, 5), point(-2, 3, 1))).toBe(false);
 });
