8f8122c9aa5859604186e6e941f5e4320a6c854b
swiftaff <swiftaff@gmail.com>
2020-03-05 21:23:52 +0800

    lights

--- src/lights.js
+++ src/lights.js
@@ -1,7 +1,45 @@
+const {
+    color,
+    colors_multiply,
+    color_scalarMultiply,
+    colors_add,
+    tuple_subtract,
+    vector_normalize,
+    vector_dotProduct,
+    tuple_multiply,
+    reflect
+} = require("../src/tuples.js");
+
 function point_light(position, intensity) {
     return { position, intensity };
 }
 
+function lighting(material, light, point, eyev, normalv) {
+    let black = color(0, 0, 0);
+    let diffuse = black;
+    let specular = black;
+    let reflectv;
+    let reflect_dot_eye;
+
+    let effective_color = colors_multiply(material.color, light.intensity);
+    let lightv = vector_normalize(tuple_subtract(light.position, point));
+    let ambient = color_scalarMultiply(effective_color, material.ambient);
+    let light_dot_normal = vector_dotProduct(lightv, normalv);
+
+    if (light_dot_normal >= 0) {
+        diffuse = color_scalarMultiply(color_scalarMultiply(effective_color, material.diffuse), light_dot_normal);
+        reflectv = reflect(tuple_multiply(lightv, -1), normalv);
+        reflect_dot_eye = vector_dotProduct(reflectv, eyev);
+        if (reflect_dot_eye > 0) {
+            let factor = Math.pow(reflect_dot_eye, material.shininess);
+
+            specular = color_scalarMultiply(light.intensity, material.specular * factor);
+        }
+    }
+    return colors_add(colors_add(ambient, diffuse), specular);
+}
+
 module.exports = {
-    point_light
+    point_light,
+    lighting
 };
--- test/materials.test.js
+++ test/materials.test.js
@@ -1,5 +1,6 @@
 const { material } = require("../src/materials.js");
-const { color } = require("../src/tuples.js");
+const { color, vector, point, getBool_colorsAreEqual } = require("../src/tuples.js");
+const { point_light, lighting } = require("../src/lights.js");
 
 test("The default material", function() {
     let m = material();
@@ -8,4 +9,47 @@ test("The default material", function() {
     expect(m.diffuse).toBe(0.9);
     expect(m.specular).toBe(0.9);
     expect(m.shininess).toBe(200);
+});
+
+let m = material();
+let position = point(0, 0, 0);
+
+test("Lighting with the eye between the light and the surface", function() {
+    let eyev = vector(0, 0, -1);
+    let normalv = vector(0, 0, -1);
+    let light = point_light(point(0, 0, -10), color(1, 1, 1));
+    let result = lighting(m, light, position, eyev, normalv);
+    expect(result).toMatchObject(color(1.9, 1.9, 1.9));
+});
+
+test("Lighting with the eye between light and surface, eye offset 45°", function() {
+    let eyev = vector(0, Math.sqrt(2) / 2, -Math.sqrt(2) / 2);
+    let normalv = vector(0, 0, -1);
+    let light = point_light(point(0, 0, -10), color(1, 1, 1));
+    let result = lighting(m, light, position, eyev, normalv);
+    expect(result).toMatchObject(color(1, 1, 1));
+});
+
+test("Lighting with eye opposite surface, light offset 45°", function() {
+    let eyev = vector(0, 0, -1);
+    let normalv = vector(0, 0, -1);
+    let light = point_light(point(0, 10, -10), color(1, 1, 1));
+    let result = lighting(m, light, position, eyev, normalv);
+    expect(getBool_colorsAreEqual(result, color(0.7364, 0.7364, 0.7364))).toBe(true);
+});
+
+test("Lighting with eye in the path of the reflection vector", function() {
+    let eyev = vector(0, -Math.sqrt(2) / 2, -Math.sqrt(2) / 2);
+    let normalv = vector(0, 0, -1);
+    let light = point_light(point(0, 10, -10), color(1, 1, 1));
+    let result = lighting(m, light, position, eyev, normalv);
+    expect(getBool_colorsAreEqual(result, color(1.6364, 1.6364, 1.6364))).toBe(true);
+});
+
+test("Lighting with the light behind the surface", function() {
+    let eyev = vector(0, 0, -1);
+    let normalv = vector(0, 0, -1);
+    let light = point_light(point(0, 0, 10), color(1, 1, 1));
+    let result = lighting(m, light, position, eyev, normalv);
+    expect(result).toMatchObject(color(0.1, 0.1, 0.1));
 });
