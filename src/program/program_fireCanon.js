5a4fd9937795e877604a1357c4222e97a9940ff1
swiftaff <swiftaff@gmail.com>
2020-03-07 23:55:20 +0800

    timing to compare with rust

--- src/program/program_fireCanon.js
+++ src/program/program_fireCanon.js
@@ -4,9 +4,11 @@ const { pixelCanvas, pixel_write, pixelCanvas_to_ppm } = require("../canvas.js")
 
 const fs = require("fs");
 
+const { performance } = require("perf_hooks");
+
 function fireCanon() {
     console.log("fireCanon");
-    let c = pixelCanvas(500, 500);
+    //let c = pixelCanvas(500, 500);
     let orange = color(1, 1, 0);
     // velocity is normalized to 1 unit/tick.
     let proj = projectile(point(0, 1, 0), vector(5, 10, 0));
@@ -20,17 +22,19 @@ function fireCanon() {
     console.log("Environment Wind", env.wind);
     console.log(" ");
 
-    while (proj.position.y > 0 && c) {
-        process.stdout.write(
-            "Tick: " + tickCount + ". Projectile Position (" + proj.position.x + "," + proj.position.y + ")\r"
-        );
-        let newC = pixel_write(c, Math.floor(proj.position.x), Math.floor(c.height - proj.position.y), orange);
-        c = newC;
+    var t0 = performance.now();
+    while (proj.position.y > 0) {
+        //console.log("Tick: " + tickCount + ". Projectile Position (" + proj.position.x + "," + proj.position.y + ")\r");
+        //let newC = pixel_write(c, Math.floor(proj.position.x), Math.floor(c.height - proj.position.y), orange);
+        //c = newC;
         tickCount++;
         orange.red = orange.red - 0.01;
         orange.green = orange.green - 0.01;
         proj = tick(env, proj);
     }
+    var t1 = performance.now();
+    console.log("Ticks: " + tickCount + ".  Time elapsed is: " + (t1 - t0) + " milliseconds.");
+    /*
     let ppm = pixelCanvas_to_ppm(c);
 
     fs.writeFile("./public/images/fireCanonImage.ppm", ppm, function(err) {
@@ -39,6 +43,7 @@ function fireCanon() {
         process.stdout.write("\r");
         console.log("Successfully wrote to the file!");
     });
+    */
 }
 
 fireCanon();
